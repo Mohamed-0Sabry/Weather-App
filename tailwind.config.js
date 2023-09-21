94cf4f3a9bf7c8a22980d373f6cb83771bcb99ed
Mohamed-0Sabry <85449133+Mohamed-0Sabry@users.noreply.github.com>
2023-09-21 08:07:34 +0300

    m

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        firstcolor: "#635985",
+        secondcolor: "#443C68",
+        thirdcolor: "#393053",
+        fourthcolor: "#18122B",
+        basecolor: "#A3C7D6",
+      },
+    },
+  },
+  plugins: [],
+};
